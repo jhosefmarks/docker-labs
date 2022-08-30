2be637d4eba92e23ddb511d3236937eed3fd61cc
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-08-30 16:22:19 -0300

    labs-05

--- node/index.js
+++ node/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Jose')`
+connection.query(sql)
+
+connection.end()
 
 app.get('/', (_, res) => {
-  res.send('<h1>Docker + NodeJS = 😍</h1>')
+  res.send('<h1>Docker + NodeJS === 😍</h1>')
 })
 
 app.listen(port, () => {
